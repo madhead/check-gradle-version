4fddc8602d244c962009521d2835a737bf83d0e7
madhead <siarhei.krukau@gmail.com>
2020-10-04 22:20:16 +0300

    Trying to execute gradlew

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import * as core from '@actions/core'
 import * as path from 'path'
 import axios from 'axios'
-// import util from 'util'
-// import { exec as callbackExec } from 'child_process'
+import util from 'util'
+import {exec as callbackExec} from 'child_process'
 
-// const exec = util.promisify(callbackExec)
+const exec = util.promisify(callbackExec)
 
 async function run(): Promise<void> {
   try {
@@ -12,10 +12,10 @@ async function run(): Promise<void> {
 
     core.info(`gradlew executable: ${gradlew}`)
 
-    // const output = await exec(`${gradlew} --version`)
+    const output = await exec(`${gradlew} --version`)
 
-    // core.info(output.stdout)
-    // core.info(output.stderr)
+    core.info(output.stdout)
+    core.info(output.stderr)
 
     const current = (
       await axios.get('https://services.gradle.org/versions/current')
