5cbecf7b5247e98868a99623cb7f6774d0e23a58
madhead <siarhei.krukau@gmail.com>
2020-10-04 22:04:27 +0300

    Add some code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import * as core from '@actions/core'
+import axios from 'axios'
+import util from 'util'
+import {exec as callbackExec} from 'child_process'
+
+const exec = util.promisify(callbackExec)
 
 async function run(): Promise<void> {
   try {
     const gradlew = core.getInput('gradlew')
 
-    core.info(`gradlew: ${gradlew}`)
+    const output = await exec(`${gradlew} --version`)
+
+    core.info(output.stdout)
+    core.info(output.stderr)
+
+    const current = (
+      await axios.get('https://services.gradle.org/versions/current')
+    ).data.version
+
+    core.info(`Current Gradle version: ${current}`)
   } catch (error) {
     core.setFailed(error.message)
   }
